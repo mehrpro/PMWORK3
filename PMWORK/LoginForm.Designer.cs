575603fccda54ba22928f334c0ae246aebdfe3fd
farshid mohammadi <fm708801@gmail.com>
2021-07-06 14:05:26 +0430

    3213212

--- PMWORK/LoginForm.Designer.cs
+++ PMWORK/LoginForm.Designer.cs
@@ -35,12 +35,18 @@ namespace PMWORK
             this.btnSetting = new DevExpress.XtraEditors.SimpleButton();
             this.btnClose = new DevExpress.XtraEditors.SimpleButton();
             this.formAssistant1 = new DevExpress.XtraBars.FormAssistant();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // lblUserName
             // 
             this.lblUserName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblUserName.Location = new System.Drawing.Point(435, 146);
+            this.lblUserName.Location = new System.Drawing.Point(420, 29);
             this.lblUserName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lblUserName.Name = "lblUserName";
             this.lblUserName.Size = new System.Drawing.Size(46, 13);
@@ -50,7 +56,7 @@ namespace PMWORK
             // lblPassword
             // 
             this.lblPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblPassword.Location = new System.Drawing.Point(435, 175);
+            this.lblPassword.Location = new System.Drawing.Point(420, 55);
             this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(32, 13);
@@ -59,47 +65,87 @@ namespace PMWORK
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(396, 332);
+            this.btnLogin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLogin.Location = new System.Drawing.Point(333, 80);
             this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(100, 35);
+            this.btnLogin.Size = new System.Drawing.Size(80, 25);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "ورود";
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // btnSetting
             // 
-            this.btnSetting.Location = new System.Drawing.Point(288, 332);
+            this.btnSetting.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSetting.Location = new System.Drawing.Point(245, 80);
             this.btnSetting.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSetting.Name = "btnSetting";
-            this.btnSetting.Size = new System.Drawing.Size(100, 35);
+            this.btnSetting.Size = new System.Drawing.Size(80, 25);
             this.btnSetting.TabIndex = 3;
             this.btnSetting.Text = "تنظیمات";
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(180, 332);
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(157, 80);
             this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(100, 35);
+            this.btnClose.Size = new System.Drawing.Size(80, 25);
             this.btnClose.TabIndex = 4;
             this.btnClose.Text = "بستن";
             // 
-            // frmLogin
+            // textEdit1
+            // 
+            this.textEdit1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textEdit1.Location = new System.Drawing.Point(157, 26);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Parmida_ActivePlusMultiKeys = false;
+            this.textEdit1.Size = new System.Drawing.Size(256, 20);
+            this.textEdit1.TabIndex = 5;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textEdit2.Location = new System.Drawing.Point(157, 52);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Parmida_ActivePlusMultiKeys = false;
+            this.textEdit2.Size = new System.Drawing.Size(256, 20);
+            this.textEdit2.TabIndex = 6;
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.EditValue = global::PMWORK.Properties.Resources._10460122071;
+            this.pictureEdit1.Location = new System.Drawing.Point(12, 17);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Parmida_ActivePlusMultiKeys = false;
+            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Zoom;
+            this.pictureEdit1.Size = new System.Drawing.Size(126, 111);
+            this.pictureEdit1.TabIndex = 7;
+            // 
+            // LoginForm
             // 
             this.Appearance.Options.UseFont = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 440);
+            this.ClientSize = new System.Drawing.Size(478, 140);
+            this.Controls.Add(this.pictureEdit1);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.textEdit1);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnSetting);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.lblPassword);
             this.Controls.Add(this.lblUserName);
             this.Font = new System.Drawing.Font("B Yekan", 9.75F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "frmLogin";
-            this.Text = "Login";
+            this.Name = "LoginForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "اتوماسیون نت";
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,6 +159,9 @@ namespace PMWORK
         private DevExpress.XtraEditors.SimpleButton btnSetting;
         private DevExpress.XtraEditors.SimpleButton btnClose;
         private DevExpress.XtraBars.FormAssistant formAssistant1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
     }
 }
 
